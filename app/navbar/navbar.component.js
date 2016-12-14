15492f468e42d1984e4a9880254c2199f6d18787
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2016-12-14 15:05:57 +0800

    Fix navbar bug, add order quick access

--- app/navbar/navbar.component.js
+++ app/navbar/navbar.component.js
@@ -25,7 +25,15 @@ angular.module('navbar')
                 //console.log(self.active);
                 self.active = [];
                 self.active[$param] = "active";
-            }
+            };
+            $('.button-collapse').sideNav({
+                    menuWidth: 300, // Default is 240
+                    edge: 'left', // Choose the horizontal origin
+                    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+                    draggable: true // Choose whether you can drag to open on touch screens
+                }
+            );
         }]
+    
     });
 
--- app/welcome/welcome.component.js
+++ app/welcome/welcome.component.js
@@ -57,13 +57,7 @@ angular.module('welcome')
     
             $('select').material_select();
             $('.modal').modal();
-            $('.button-collapse').sideNav({
-                    menuWidth: 300, // Default is 240
-                    edge: 'left', // Choose the horizontal origin
-                    closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-                    draggable: true // Choose whether you can drag to open on touch screens
-                }
-            );
+  
     
     
             self.doItNow = function() {
